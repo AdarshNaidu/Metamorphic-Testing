0b165ffe760bf4ada01ff227f57eb5ee8a8a4e9b
Harsh Agarwal <agrawal.harsh14@gmail.com>
2020-11-27 21:58:49 +0530

    Update youtube.js

--- test/youtube.js
+++ test/youtube.js
@@ -79,3 +79,46 @@ describe('YouTube API', async () => {
     const result = checkComplete(response1, response2, response3);
     assert.equal(result, true);
   });
+
+  it('Difference Relation', async () => {
+    const result1 = { id: 1, name: 'video1' };
+
+    mock.onPut(`${hostname}/api/videos/1`).reply(200, { id: 1, name: 'video1.1' });
+
+    const response1 = await axios.put(`${hostname}/api/videos/1`, { id: 1, name: 'video1.1' }).then((res) => res.data);
+
+    const result = checkDifference(result1, response1);
+    assert.equal(result, true);
+  });
+});
+
+const checkEquivalence = (a, b) => {
+  if (a.length !== b.length) return false;
+
+  return true;
+};
+
+const checkEquality = (a, b) => {
+  if (a.length !== b.length) return false;
+
+  if (a[0].id !== b[0].id) return false;
+
+  return true;
+};
+
+const checkSubset = (a, b) => {
+  if (b.length > a.length) return false;
+  return true;
+};
+
+const checkDisjoint = (a, b) => {
+  if (a[0].id === b[0].id) return false;
+  return true;
+};
+
+const checkComplete = (a, b, c) => {
+  if (c.length === a.length + b.length) return true;
+  return false;
+};
+
+const checkDifference = (a, b) => (JSON.stringify(a) !== JSON.stringify(b));
